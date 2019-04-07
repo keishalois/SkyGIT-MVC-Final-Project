287a21bf39360661558594503a3799be73921713
Markless <sms19@ostml0135448.mynet>
2019-04-07 16:44:04 +0100

    Fixed the link to the read page to see individual items

--- views/blogs/readAll.php
+++ views/blogs/readAll.php
@@ -3,8 +3,8 @@
 <?php foreach($blogposts as $blogpost) { ?>
   <p>
     <?php echo $blogpost->title; ?> &nbsp; &nbsp;
-    <a href='?controller=blog&action=read&id=<?php echo $blogpost->blogid; ?>'>See Blog</a> &nbsp; &nbsp;
-    <a href='?controller=blog&action=delete&id=<?php echo $blogpost->blogid; ?>'>Delete Blog</a> &nbsp; &nbsp;
-    <a href='?controller=blog&action=update&id=<?php echo $blogpost->blogid; ?>'>Amend Blog</a> &nbsp;
+    <a href='?controller=blog&action=read&blogid=<?php echo $blogpost->blogid; ?>'>See Blog</a> &nbsp; &nbsp;
+    <a href='?controller=blog&action=delete&blogid=<?php echo $blogpost->blogid; ?>'>Delete Blog</a> &nbsp; &nbsp;
+    <a href='?controller=blog&action=update&blogid=<?php echo $blogpost->blogid; ?>'>Amend Blog</a> &nbsp;
   </p>
 <?php } ?>

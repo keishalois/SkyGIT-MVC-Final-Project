babe1bbd22c7ada117ec6d248dcf074b0567e257
cathyleeson <cathyleeson17@gmail.com>
2019-04-08 16:06:48 +0100

    Style for landing, sign up and log in

--- controllers/blog_controller.php
+++ controllers/blog_controller.php
@@ -10,9 +10,9 @@ class BlogController {
     public function read() {
       // we expect a url of form ?controller=posts&action=show&id=x
       // without an id we just redirect to the error page as we need the post id to find it in the database
-      if (!isset($_GET['blogid'])){
+      if (!isset($_GET['blogid']))
         return call('pages', 'error');
-      }
+
       try{
       // we use the given id to get the correct post
       $blogpost = BlogPost::find($_GET['blogid']);
@@ -38,28 +38,27 @@ class BlogController {
       }
       
     }
+    public function update() {
+        
+      if($_SERVER['REQUEST_METHOD'] == 'GET'){
+          if (!isset($_GET['blogid']))
+        return call('pages', 'error');
+
+        // we use the given id to get the correct product
+        $blogpost = BlogPost::find($_GET['blogid']);
+      
+        require_once('views/blogs/update.php');
+        }
+      else
+          { 
+            $blogid = $_GET['blogid'];
+            BlogPost::update($blogid);
+                        
+            $blogposts = BlogPost::all();
+            require_once('views/blogs/readAll.php');
+      }
+    }
 }
-//    public function update() {
-//        
-//      if($_SERVER['REQUEST_METHOD'] == 'GET'){
-//          if (!isset($_GET['id']))
-//        return call('pages', 'error');
-//
-//        // we use the given id to get the correct product
-//        $product = Product::find($_GET['id']);
-//      
-//        require_once('views/products/update.php');
-//        }
-//      else
-//          { 
-//            $id = $_GET['id'];
-//            Product::update($id);
-//                        
-//            $products = Product::all();
-//            require_once('views/products/readAll.php');
-//      }
-//      
-//    }
 //    public function delete() {
 //            Product::remove($_GET['id']);
 //            
@@ -69,4 +68,5 @@ class BlogController {
 //      
 //    }
 //  
+//
 //?>

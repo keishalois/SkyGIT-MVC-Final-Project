fb55e1039706ec770d4f4f0da7d295b5ea13517d
Keisha Hunt <keishahunt@keishas-macbook.home>
2019-04-08 21:46:18 +0100

    rewrote user methods to remove log in with email

--- models/user.php
+++ models/user.php
@@ -9,18 +9,18 @@ class User {
     protected $password;
     protected $usertype;
 	
-        public function __construct($email, $username, $password) {
-                $this->email = $email;
+        public function __construct($username, $password) {
+//                $this->email = $email;
 		$this->username = $username;
                 $this->password = $password;
 	}
         
         public function loginUser() {
         $db = Db::getInstance();        
-        $sql = "SELECT Username, Password FROM users WHERE Username= :user AND Password= :psw";
+        $sql = "SELECT Username, Password FROM users WHERE Username= :username AND Password= :psw";
         try {
             $stmt = $db->prepare($sql);
-            $stmt->execute(['user'=> $this->username, 'psw'=> $this->password]);
+            $stmt->execute(['username'=> $this->username, 'psw'=> $this->password]);
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 $_SESSION["username"]=$row["Username"];
                     header("Location:../../index.php");}
@@ -32,19 +32,27 @@ class User {
         unset($stmt);
     }
     
-        public function createUser() {
+            public function createUser() {
         $db = Db::getInstance();        
-        $sql = "INSERT INTO users (Email, Username, Password, UserType) VALUES (:email, :user, :psw, 'blogger')";
+        $req = $db->prepare( "INSERT INTO users (Email, Username, Password, UserType) VALUES (:email, :username, :password, 'blogger')");
+            $req->bindParam(':username', $username);
+            $req->bindParam(':password', $password);
+            $req->bindParam(':email', $email);
         try {
-            $stmt = $db->prepare($sql);
-            $stmt->execute(['email'=> $this->email, 'user'=> $this->username, 'psw'=> $this->password]);
-                }
-
+    if(isset($_POST['username'])&& $_POST['username']!=""){
+       $username = $filteredUser = filter_input(INPUT_POST,'username', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    if(isset($_POST['password'])&& $_POST['password']!=""){
+        $password = $filteredContent = filter_input(INPUT_POST,'password', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+    if(isset($_POST['email'])&& $_POST['email']!=""){
+       $email = $filteredEmail = filter_input(INPUT_POST,'email', FILTER_SANITIZE_SPECIAL_CHARS);
+    }
+$req->execute(); }
         catch (PDOException $e) {
             $error = $e->errorInfo();
-            die("Sign up failed sorry ..." . $error . $e->getMessage());
-        }
-        unset($stmt);
+            die("Sign up failed sorry ..." . $error . $e->getMessage()); }
+        unset($req);
     }
        
         }

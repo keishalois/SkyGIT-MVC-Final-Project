76d429b38cd958e139277ed081a2720ed838838b
cathyleeson <cathyleeson17@gmail.com>
2019-04-10 20:01:40 +0100

    Add files via upload

---
+++ create.php
@@ -0,0 +1,29 @@
+<div class="add-blog">
+<p>Fill in the following form to create a new product:</p>
+
+<form action="" method="POST" class="w3-container" id="addform" enctype="multipart/form-data">
+
+   <h2>Add New Blog</h2>
+   <div class ="add-form">
+   <p>
+       <label>Title</label><br>
+       <textarea rows="1" cols="100%" name="title" form="addform"></textarea>
+   </p>
+
+   <p>
+       <label>Content</label><br>
+       <textarea rows="10" cols="100" name="content" form="addform"></textarea>
+   </p>
+ <input type="hidden"
+       name="MAX_FILE_SIZE"
+        value="10000000"
+        />
+
+ <input type="file" name="myUploader" class="w3-btn" required />
+ <p>
+   <input class="w3-btn" type="submit" value="Add Blog Post">
+ </p>
+   </div>
+</form>
+</div>
+<br>

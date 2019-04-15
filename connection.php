37224c761793dcca978f4bf8e18d7a91295b15f7
keishalois <keisha.hunt93@hotmail.com>
2019-04-15 21:28:42 +0100

    testing something

--- connection.php
+++ connection.php
@@ -8,7 +8,18 @@ class DB {
     public static function getInstance() {
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-        self::$instance = new PDO('mysql:host=localhost;dbname=fml_fp', 'root', '', $pdo_options);
+        
+        $db_host = ini_get('db_host');
+        $db_name = ini_get('db_name');
+        $db_user = ini_get('db_user');
+        $db_password = ini_get('db_password');
+                
+        if(empty($db_host) || empty($db_name) ||  empty($db_user) || empty($db_password)) {        
+            self::$instance = new PDO('mysql:host=localhost;dbname=fml_fp', 'root', '', $pdo_options);
+        } else {
+            self::$instance = new PDO("mysql:host=$db_host;dbname=$db_name",$db_user,$db_password, $pdo_options);
+        } 
+  
       }
       return self::$instance;
     }

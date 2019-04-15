256c8273557a2e6a5b81c274f5f593a584765268
keishalois <keisha.hunt93@hotmail.com>
2019-04-15 21:35:24 +0100

    testing

--- connection.php
+++ connection.php
@@ -9,10 +9,10 @@ class DB {
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
         
-        $db_host = env('db_host');
-        $db_name = env('db_name');
-        $db_user = env('db_user');
-        $db_password = env('db_password');
+        $db_host = getenv('db_host');
+        $db_name = getenv('db_name');
+        $db_user = getenv('db_user');
+        $db_password = getenv('db_password');
                 
         if(empty($db_host) || empty($db_name) ||  empty($db_user) || empty($db_password)) {        
             self::$instance = new PDO('mysql:host=localhost;dbname=fml_fp', 'root', '', $pdo_options);

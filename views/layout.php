1b1f43cdeec184c9acdd02d8bbeff98f690569b0
keishalois <keisha.hunt93@hotmail.com>
2019-04-20 14:56:04 +0100

    moves jquery for file upload name to show

--- views/layout.php
+++ views/layout.php
@@ -25,6 +25,7 @@
         <script src="views/javascript/deleteBlog.js"></script>
         <script src="views/javascript/updateBlog.js"></script>
         <script src="views/javascript/comments.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         
 <title>Fork My Life</title>
 <link rel="icon" type="image/png" href="images/standard/header-fml.png">
@@ -69,7 +70,6 @@
                 </footer>
             </div>-->
 <!-- scripts required for bootstrap -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
   </body>

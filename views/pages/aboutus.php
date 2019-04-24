187998cc2ff886a44d532ba8beafd3219cfbd319
Markless <sms19@ostml0135448.mynet>
2019-04-24 16:40:39 +0100

    Making Profile format consistent with other pages

--- views/pages/aboutus.php
+++ views/pages/aboutus.php
@@ -1,8 +1,9 @@
 
 
-
-<div class="rounded mx-auto" id="aboutus" align="center">
-        <h3>About us!</h3>
+<br>
+<div class="add-blog" id="aboutus">
+    <br>
+        <h2>About us!</h2>
         <p>We are the creators of Fork My Life!<p>
         <img src ="views/images/standard/A-team.jpg" height=50% width=70%/>
     </div>

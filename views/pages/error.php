a2ca64122d09ba09b3f5bcb33f2a476aaaee0a61
CharlotteMeigh <charlottemeigh88@gmail.com>
2019-04-25 15:05:31 +0100

    Show revised copy.

--- views/pages/error.php
+++ views/pages/error.php
@@ -1,5 +1,5 @@
 <div class="w3-container w3-red">
-<p>Oops, this is the error page.</p>
+<p>Eek, this is the error page!</p>
 <img src="views/images/standard/_errorimage.jpg" width="150" />
-<p>It looks like something went wrong :(</p>
+<p>Our bad, looks like something went wrong :( Try going Home again, Dorothy.</p>
 </div>

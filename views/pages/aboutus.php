9dc5acdf609c988fd55d38c298ffc4072d9e1e57
CharlotteMeigh <charlottemeigh88@gmail.com>
2019-04-25 14:47:31 +0100

    Show revised copy.

--- views/pages/aboutus.php
+++ views/pages/aboutus.php
@@ -4,7 +4,7 @@
 <div class="add-blog" id="aboutus">
     <br>
         <h2>About us!</h2>
-        <p>We are the creators of Fork My Life!<p>
+        <p>Hiya! We're the creators of Fork My Life! Woooooo!<p>
         <img src ="views/images/standard/A-team.jpg" height=50% width=70%/>
     </div>
 

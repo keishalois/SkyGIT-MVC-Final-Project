26a64ad04412f465272d17a76c453f2d234e1243
CharlotteMeigh <charlottemeigh88@gmail.com>
2019-04-25 15:24:15 +0100

    Show revised copy.

--- views/chatlayout.php
+++ views/chatlayout.php
@@ -21,7 +21,7 @@
 <!-- javascript links for blog page -->
         <script src="/views/javascript/chatbox.js"></script>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<title>Live Chat - Fork My Life</title>
+<title>Live Chat – Fork My Life</title>
   </head>
   <body>
       <div class="w3-container">
